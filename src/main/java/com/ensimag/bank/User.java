7cb723079e1c0d148dcc7147bdc5f5cf67de47d4
Nolwenn Cadic <nolwenn.cadic@grenoble-inp.org>
2018-11-08 10:27:42 +0100

    arborescence

---
+++ src/main/java/com/ensimag/bank/User.java
@@ -0,0 +1,7 @@
+package com.ensimag.bank;
+
+/**
+ * Created by cadicn on 11/8/18.
+ */
+public class User {
+}
---
+++ src/main/java/com/ensimag/message/Message$.java
@@ -0,0 +1,7 @@
+package com.ensimag.message;
+
+/**
+ * Created by cadicn on 11/8/18.
+ */
+public class Message$ {
+}
